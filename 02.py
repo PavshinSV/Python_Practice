5e061c59fcac46e0a27b6b9cf38c1949fe7b0b94
PSV <pavshin@mail.ru>
2022-04-15 02:30:04 +0700

    02 - решено

---
+++ 02.py
@@ -0,0 +1,11 @@
+# Найти максимальное из пяти чисел
+import numbers
+
+amount = 5
+number = []
+for i in range(0,amount):
+    number.append((int(input(f'Введите число №{i+1}: '))))
+print()
+print(f'Вы ввели следующие числа: {number}')
+print()
+print(f'Максимальным числом является: {max(number)}')
