6cdca1938ef14d6879c80820fdd2b0200f44a100
PSV <pavshin@mail.ru>
2022-04-15 22:12:22 +0700

    07 - решено

---
+++ 07.py
@@ -0,0 +1,10 @@
+# Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
+
+value = [0,1]
+print(f'X  Y  Z  \tX V Y V Z\t¬X ⋀ ¬Y ⋀ ¬Z\tX V Y V Z = ¬X ⋀ ¬Y ⋀ ¬Z')
+for x in value:
+    for y in value:
+        for z in value:
+            first_expression = bool(x or y or z)
+            second_expression = (not x) and (not y) and (not z)
+            print(f'{x}  {y}  {z}  \t{first_expression}\t\t{second_expression}\t\t{first_expression==second_expression}')
