c1e2da13ed670d3dedbf0b68771a70b343c85893
PSV <pavshin@mail.ru>
2022-04-19 01:02:13 +0700

    07-исправил ошибку в логическом выражении, забыл отрицание

--- 07.py
+++ 07.py
@@ -1,11 +1,11 @@
 # Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
 
 value = [0, 1]
-print(f'X  Y  Z  \tX V Y V Z\t¬X ⋀ ¬Y ⋀ ¬Z\tX V Y V Z = ¬X ⋀ ¬Y ⋀ ¬Z')
+print(f'¬(X  Y  Z)  \tX V Y V Z\t¬X ⋀ ¬Y ⋀ ¬Z\tX V Y V Z = ¬X ⋀ ¬Y ⋀ ¬Z')
 for x in value:
     for y in value:
         for z in value:
-            first_expression = bool(x or y or z)
+            first_expression = bool(not(x or y or z))
             second_expression = (not x) and (not y) and (not z)
             print(
                 f'{x}  {y}  {z}  \t{first_expression}\t\t{second_expression}\t\t{first_expression==second_expression}')
