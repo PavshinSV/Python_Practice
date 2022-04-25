97beb83f478611711e88f2599f25be105dea7354
Сергей Павшин <Pavshin@enistro.ru>
2022-04-25 13:20:50 +0700

    22 - решена

--- 22.py
+++ 22.py
@@ -1,1 +1,8 @@
 # Найти сумму чисел списка стоящих на нечетной позиции
+
+list_of_numbers=[0,25,135,-32,98,12,-83,2]
+summ=0
+for i in range(0,len(list_of_numbers)-1,2):
+    summ+=list_of_numbers[i]
+print(f'В имеющемся массиве {list_of_numbers}')
+print(f'Сумма элементов стоящих на нечетных позициях равна: {summ}')
