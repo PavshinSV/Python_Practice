1f58039dc78b4059c882b185795d84611db0d887
PSV <pavshin@mail.ru>
2022-04-25 20:55:11 +0700

    25 - отформатировал, все время забываю

--- 25.py
+++ 25.py
@@ -1,14 +1,16 @@
 # Написать программу преобразования десятичного числа в двоичное
 
 def decimal_at_binary(num):
-    result=''
-    while num>1:
-        result=str(num%2)+result
-        num=int(num/2)
-    if num==1:
-        result='1'+result
+    result = ''
+    while num > 1:
+        result = str(num % 2)+result
+        num = int(num/2)
+    if num == 1:
+        result = '1'+result
     return result
 
-number=int(input('Введите целое десятичное число: '))
 
-print(f'В двоичном формате это число запишется в виде: {decimal_at_binary(number)}')
+number = int(input('Введите целое десятичное число: '))
+
+print(
+    f'В двоичном формате это число запишется в виде: {decimal_at_binary(number)}')
