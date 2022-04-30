985700838d14a8543e5c2e90dcdf65acc4feccb6
PSV <pavshin@mail.ru>
2022-04-30 20:12:36 +0700

    28 - переделана и закончена

--- 28.py
+++ 28.py
@@ -5,18 +5,25 @@
 def deskriminanta(a, b, c):
     return b**2-4*a*c
 
-# (-b-+(d)**0.5)/2a
+def square_roots(a, b, c):
+    d = deskriminanta(a, b, c)
+    if d < 0:
+        print(f'D < 0. Система не имеет решений!\n')
+    elif d > 0:
+        print(
+            f'D > 0. Система имеет два корня, х1: {(-b-d**0.5)/(2*a):.2f} и х2: {(-b+d**0.5)/(2*a):.2f}\n')
+    else:
+        print(f'D = 0. Система имеет один корень, х: {-b/(2*a):.2f}\n')
 
 
 a = 4.5
 b = 6
 c = 2
 
-d = deskriminanta(a, b, c)
-if d < 0:
-    print(f'D < 0. Система не имеет решений!\n')
-elif d > 0:
-    print(
-        f'D > 0. Система имеет два корня, х1: {(-b-d**0.5)/(2*a):.2f} и х2: {(-b+d**0.5)/(2*a):.2f}\n')
-else:
-    print(f'D = 0. Система имеет один корень, х: {-b/(2*a):.2f}\n')
+square_roots(a,b,c) 
+
+# второй способ
+
+import algebra
+
+algebra.square_roots(a,b,c)
