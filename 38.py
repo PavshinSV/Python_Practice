f52a83c3dba204d4a1472e7742643684e73c48d2
Pavshin Sergei <pavshin@mail.ru>
2022-05-18 16:38:06 +0700

    38 - решена

--- 38.py
+++ 38.py
@@ -1,1 +1,12 @@
 # 38. Напишите программу, удаляющую из текста все слова содержащие "абв".
+source_string = "абырабввалг Сегодня 1Абв мы strаБвншышпо будем АБВ12 изучать абВser азбуку"
+words = source_string.split(' ')
+
+print(f'Первоначально имеем строку:\n{source_string}\n')
+
+sub_string = 'абв'
+
+for word in words:
+    if word.lower().find(sub_string) != -1: words.remove(word)
+
+print(f'После удаления слов содержащих искомую подстроку получаем:\n{" ".join(words)}')
