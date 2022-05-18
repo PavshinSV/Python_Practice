9d31a90274bc744b4a727fa9e499acab6cb97098
PSV <pavshin@mail.ru>
2022-05-18 11:25:06 +0700

    37-debugging

--- 37.py
+++ 37.py
@@ -2,3 +2,34 @@
 # Пример: [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3, 4, 6, 7]
 #   [5, 2, 3, 4, 6, 1, 7] => [2, 3, 4, 6, 7]
 # Порядок элементов менять нельзя
+
+n = [5, 2, 3, 4, 6, 1, 7]
+result = []
+length = len(n)
+indexes = []
+
+for j in range(0, (length - 1)):
+    new_list = True
+    while new_list:
+        r = []
+        new_list = False
+        for i in range(j, length):
+            if not i:
+                r.append(n[i])
+            if len(r) == 1:
+                if not indexes.count(i) and n[i] > r[-1]:
+                    r.append(n[i])
+                    indexes.append(i)
+                    new_list = True
+            elif n[i] > r[-1]:
+                r.append(n[i])
+        if new_list: result.append(r)
+
+maxi = []
+
+for e in result:
+    if len(e) > len(maxi):
+        maxi = e.copy()
+
+print(
+    f'Из имеющегося списка {n} можно составить следующие возрастающие последовательности: \n{result},\n при этом самой длиной последовательностью явялется {maxi} ')
