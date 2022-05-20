d1c2a86036cb2635e787bf843ee54c8a396d7989
PSV <pavshin@mail.ru>
2022-05-20 22:09:19 +0700

    11_review - решена

--- 11_review.py
+++ 11_review.py
@@ -1,4 +1,8 @@
 # 11.	Сформировать список из  N членов последовательности.
 # Для N = 5: 1, -3, 9, -27, 81 и т.д.
 # c помощью использования лямбд, filter, map, zip, enumerate, list comprehension:
+import random
 
+n=random.randint(1,20)
+test_list=[x for x in range(-n,n+1)]
+print(f'Для N = {n} получаем список:\n{test_list}')
