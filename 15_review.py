787635cd99b5ec8d6f5e2e8213a155bd500a0505
PSV <pavshin@mail.ru>
2022-05-20 23:11:59 +0700

    15_review - решена

--- 15_review.py
+++ 15_review.py
@@ -2,3 +2,10 @@
 # Пример: пусть N = 4, тогда
 # [1, 2, 6, 24]
 # c помощью использования лямбд, filter, map, zip, enumerate, list comprehension:
+import math
+import random
+
+n=random.randint(2,10)
+
+test = [math.factorial(x) for x in range(1,n+1)]
+print(f'Для N = {n} набор произведений чисел от 1 до N:\n{test}')
