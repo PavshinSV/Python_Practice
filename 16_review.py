2106429d7b56bf3ed05113ebf83efa60cc1c00b8
PSV <pavshin@mail.ru>
2022-05-21 00:05:11 +0700

    16_review - решена

--- 16_review.py
+++ 16_review.py
@@ -1,2 +1,9 @@
-# 16.	Задать список из n чисел последовательности  и вывести на экран их сумму.
+# 16.	Задать список из n чисел последовательности (1+1/n)^n и вывести на экран их сумму.
 # c помощью использования лямбд, filter, map, zip, enumerate, list comprehension:
+
+import random
+
+n=random.randint(1,10)
+test_list=[(1+1/x)**x for x in range(1,n+1)]
+summ=sum(test_list)
+print(f'Для N = {n} список значений {test_list},\nИх сумма равна {summ}')
