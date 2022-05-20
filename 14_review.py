18a2c0f90eb87a570636a8e8ccb7fb81b6ffdb99
PSV <pavshin@mail.ru>
2022-05-20 22:34:01 +0700

    14_review - решена

--- 14_review.py
+++ 14_review.py
@@ -1,2 +1,9 @@
 # 14.	Подсчитать сумму цифр в вещественном числе.
 # c помощью использования лямбд, filter, map, zip, enumerate, list comprehension:
+import random
+
+number=random.random()
+print(number)
+numbers=str(number).split('.')
+sum=sum(map(int, numbers[0]))+sum(map(int, numbers[1]))
+print(f'Для числа {number} сумма его цифр равна {sum}')
