bdc108bc8645f81cc3926d304dc664e5fc846ee4
PSV <pavshin@mail.ru>
2022-05-20 20:36:53 +0700

    42 - решена со скобками. Любое количество скобок (главное чтобы число открытых было равно числу закрытых) и любого количества действий

--- 42.py
+++ 42.py
@@ -12,14 +12,14 @@
 def get_operation(string):
     if '+' in string or '-' in string:
         index_add = string.find('+')
-        index_sub = string.find('/')
+        index_sub = string.find('-')
         slice_index = 0
-        if index_add < 0 or (index_sub < index_add and index_sub > -1):
+        if index_add < 0 or (index_sub > index_add and index_sub > -1):
             slice_index = index_sub
             a = get_operation(string[0:slice_index])
             b = get_operation(string[slice_index + 1:])
             return a - b
-        elif index_sub < 0 or (index_sub > index_add and index_add > -1):
+        elif index_sub < 0 or (index_sub < index_add and index_add > -1):
             slice_index = index_add
             a = get_operation(string[0:slice_index])
             b = get_operation(string[slice_index + 1:])
@@ -28,12 +28,12 @@ def get_operation(string):
         index_mult = string.find('*')
         index_div = string.find('/')
         slice_index = 0
-        if index_mult < 0 or (index_div < index_mult and index_div > -1):
+        if index_mult < 0 or (index_div > index_mult and index_div > -1):
             slice_index = index_div
             a = get_operation(string[0:slice_index])
             b = get_operation(string[slice_index + 1:])
             return a / b
-        elif index_div < 0 or (index_div > index_mult and index_mult > -1):
+        elif index_div < 0 or (index_div < index_mult and index_mult > -1):
             slice_index = index_mult
             a = get_operation(string[0:slice_index])
             b = get_operation(string[slice_index + 1:])
@@ -41,6 +41,20 @@ def get_operation(string):
     else:
         return float(string)
 
+def get_brackets(string):
+    while '(' in string:
+        open_bracket_index=string.find('(')
+#        print(open_bracket_index,type(open_bracket_index))
+        close_bracket_index=string.find(')')
+#        print(close_bracket_index, type(close_bracket_index))
+        while '(' in string[open_bracket_index+1:close_bracket_index]:
+            open_bracket_index=string.find('(',open_bracket_index+1,close_bracket_index)
+        string=string[:open_bracket_index]+str(get_operation(string[open_bracket_index+1:close_bracket_index]))+string[close_bracket_index+1:]
+#        print(string)
+    return string
 
 input_string = input("Введите в строку математическое выражение. Например 1+2*3+2*6*7+3*4:\n")
+if '(' in input_string:
+    input_string = get_brackets(input_string)
+
 print(f'Результат выражения = {get_operation(input_string)}')
